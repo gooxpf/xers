13c53e8b736666b96d858b800bfe1a461d0b5946
gooxpf <gooxpf@gmail.com>
2021-08-28 01:27:30 +0400

    create app wrapper, update react and react dom to 18 alpha

---
+++ src/components/init/AppWrapper/AppWrapper.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppWrapper: React.FC = (): React.ReactElement => {
+    return (
+        <div className="app-wrapper">
+            App Wrapper
+        </div>
+    );
+}
+
+export default AppWrapper;
